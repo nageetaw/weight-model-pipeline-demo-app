f2979719e3d9c58a64981b3cf5685594542eb4dc
nageetaw <nigeetawadhwani@gmail.com>
2025-11-06 18:01:47 +0100

    tested

--- node-backend/index.js
+++ node-backend/index.js
@@ -4,7 +4,11 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
+const cors = require("cors");
+app.use(cors({ origin: "http://localhost:3000", methods: ["GET", "POST"] }));
+
 const PY_API = process.env.PY_API;
+// const PY_API = "http://127.0.0.1:8000";
 if (!PY_API) console.warn("Warning: PY_API not set");
 
 app.get("/", (req, res) =>
@@ -14,7 +18,8 @@ app.get("/", (req, res) =>
 app.post("/api/predict", async (req, res) => {
   try {
     const { height } = req.body;
-    const r = await axios.post(`${PY_API}/predict`, { height });
+    console.log(PY_API);
+    const r = await axios.post(`${PY_API}/api/predict`, { height });
     return res.json(r.data);
   } catch (e) {
     console.error("Prediction error:", e.message);
