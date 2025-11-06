1bf6bf7ba4265c790b29b9ff79a685b25a383d80
nageetaw <nigeetawadhwani@gmail.com>
2025-11-06 16:42:04 +0100

    rewrite files name

--- python-server/main.py
+++ python-server/main.py
@@ -31,3 +31,7 @@ def predict(data: InputData):
     pred = model.predict(h)
     return {"height": data.height, "predicted_weight": float(pred[0])}
 
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
